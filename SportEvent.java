80a95f7c809a5824dda628ac2230ef313cd44199
AhmadJonaid <afloneter@gmail.com>
2026-05-02 22:49:46 +0300

    Update SportEvent.java

--- SportEvent.java
+++ SportEvent.java
@@ -3,19 +3,28 @@ public class SportEvent extends Event{
 	private String sportType;
 	private int numberOfTeams;
 	private int numberOfParticipants;
-	public SportEvent(String name, int startTime, int endTime, String venue,String department,String sportType,int numberOfTeams,int numberOfParticipants){
+	public SportEvent(String name, String startTime, String endTime, Venue venue,Department department,String sportType,int numberOfTeams,int numberOfParticipants){
 		super(name , startTime, endTime, venue, department);
 		this.sportType = sportType;
 		this.numberOfTeams = numberOfTeams;
 		this.numberOfParticipants = numberOfParticipants;
 	}
+	public String getSportType(){
+		return sportType;
+	}
+	public int getNumberOfTeams(){
+		return numberOfTeams;
+	}
+	public int getNumberOfParticipants(){
+		return numberOfParticipants;
+	}
 	//override the toString method 
-	public String toString() {
-		
-		return "ُُEvent name: " + getName() +"\nDepartment: " + getDepartment()+  
-		 "\nVenue: "+ getVenue() +"\nStart Time: " + getStartTime() + "\nEndTime: " 
-				+ getEndTime()+ "\nSport type: " + sportType +  "\nNumber of teams: " 
-		+ numberOfTeams +  "\nParticipants: " + numberOfParticipants;
+	@Override
+	public void showEventInfo() {
+		showBasicEventinfo();
+		System.out.println("Sport type :"+getSportType());
+		System.out.println("Teams numbers :"+getNumberOfTeams());
+		System.out.println("numbers of participants :"+ getNumberOfParticipants());
 	}
 	
 }
