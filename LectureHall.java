25a4b0d530b048ddec26b32c35c2c611f9323aff
AhmadJonaid <afloneter@gmail.com>
2026-05-02 15:21:14 +0300

    Refactor LectureHall class and add method overrides
    Removed unnecessary comment about deleting seats variable and added method overrides for getType and showInfo.

--- LectureHall.java
+++ LectureHall.java
@@ -1,6 +1,6 @@
 public class LectureHall extends Venue{
     private int hallNumber;
-    private int seats;
+    private int seats;//It should be deleted it simillar to Capacity
     public LectureHall(String place,String Location,int capacity,int hallNumber,int seats){
         super(place,Location,capacity);
         this.hallNumber=hallNumber;
@@ -12,5 +12,15 @@ public class LectureHall extends Venue{
     public int getSeats(){
         return seats;
     }
-    
+    @Override
+    public String getType(){
+        return "LectureHall";
+    }
+    @Override
+    public void showInfo(){
+        showBasicInfo();
+        System.out.println("Type :"+getType());
+        System.out.println("Hall Number: "+getHallNumber());
+        System.out.println("Seats Amount :"+ getSeats());// deleted?
+    }
 }
