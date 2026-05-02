72e190f3248d8c8a12928d714d43d2c28dc79a21
AhmadJonaid <afloneter@gmail.com>
2026-05-02 22:49:09 +0300

    Update SocialEvent.java

--- SocialEvent.java
+++ SocialEvent.java
@@ -1,20 +1,33 @@
-
 public class SocialEvent extends Event{
 private String  activityType;
 private boolean hasRefreshments;
 private String targetAudience;
-public SocialEvent(String name, int startTime, int endTime, String venue,String department, String activityType, boolean hasRefreshments, String targetAudience) {
+public SocialEvent(String name, String startTime, String endTime, Venue venue,Department department, String activityType, boolean hasRefreshments, String targetAudience) {
 	super(name , startTime, endTime, venue, department);
 	this.activityType = activityType;
 	this.hasRefreshments = hasRefreshments;
 	this.targetAudience = targetAudience;
 }
-//overriding the toString method
-public String toString() {
-	return "ُُEvent name: " + getName() +"\nDepartment: " + getDepartment()+  
-			 "\nVenue: "+ getVenue() +"\nStart Time: " + getStartTime() + "\nEndTime: " 
-					+ getEndTime() + "\nActivity Type: " + activityType + "\nHas Refreshments: " +
-			 hasRefreshments+ "\nTarget Audience: " + targetAudience;
+public String getActivityType(){
+    return activityType;
 }
-
+public boolean getHasRefreshments(){
+    return hasRefreshments;
+}
+public String getTargetAudience(){
+    return targetAudience;
+}
+//overriding the toString method
+@Override
+public void showEventInfo() {
+	showBasicEventinfo();
+    System.out.println("Activity type :"+getActivityType());
+    if(hasRefreshments){
+     System.out.println("Refreshments Avilibilty: Avilible");
+    }
+    else{
+        System.out.println("Refreshments Avilibilty : Not avilible");
+    }
+    System.out.println("Targted audience :"+ getTargetAudience());
+ }
 }
